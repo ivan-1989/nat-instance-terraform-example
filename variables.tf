e60c3425087d892d6f919c8c4343c22f0d751b33
Alexander Vlasov <58551180+alex-vlasov-l1@users.noreply.github.com>
2021-12-08 21:42:41 +0300

    removed token to support YC_TOKEN var assign

--- variables.tf
+++ variables.tf
@@ -1,7 +1,3 @@
-variable "token" {
-  description = "Yandex Cloud security OAuth token"
-  default     = "nope" #generate yours by this https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token
-}
 
 variable "folder_id" {
   description = "infra folder for main resources"
